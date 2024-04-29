c431ddda8326ac064f35d928fdea887a9b318cb0
Paul Rodwell <paul.rodwell@btinternet.com>
2024-04-29 16:54:15 +0100

    rewrite server in javascript
    replace formidable with multer (image plugin uses multer)
    rewrite removes need for `async` and `mkdirp`

---
+++ test/test.js
@@ -0,0 +1,17 @@
+"use strict";
+
+(function () {
+  // build time tests for assets plugin
+  // see http://mochajs.org/
+  var assets, expect;
+  assets = require('../client/assets');
+  expect = require('expect.js');
+  describe('assets plugin', function () {
+    return describe('expand', function () {});
+  });
+
+  // it 'can make itallic', ->
+  //   result = assets.expand 'hello *world*'
+  //   expect(result).to.be 'hello <i>world</i>'
+}).call(void 0);
+//# sourceMappingURL=test.js.map
