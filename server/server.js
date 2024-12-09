be4455c7621948159f7eaa50bd55842416289e6c
Paul Rodwell <paul.rodwell@btinternet.com>
2024-12-09 12:10:29 +0000

    convert to modern JavaScript
    convert client CoffeeScript to JavaScript
    convert server to module

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
 // assets plugin - server-side component
 // these handlers are launched with the wiki server.
 
-const fs = require('fs')
-const path = require('path')
-const multer = require('multer')
+import fs from 'node:fs'
+import path from 'node:path'
+import multer from 'multer'
 
-const startServer = (params) => {
+const startServer = params => {
   const { app, argv } = params
 
   const upload = multer({ dest: path.join(argv.assets, 'uploads') })
 
   const authorized = (req, res, next) => {
-
     if (app['securityhandler'].isAuthorized(req)) {
       next()
     } else {
@@ -25,51 +24,55 @@ const startServer = (params) => {
   }
 
   app.get('/plugin/assets/list', cors, (req, res) => {
-    const assetsPath = path.join(argv.assets, (req.query.assets || '').match(/([\w\/-]*)/)[1])
+    const assetsPath = path.join(argv.assets, (req.query.assets || '').match(/([\w/-]*)/)[1])
     fs.readdir(assetsPath, { withFileTypes: true }, (error, files) => {
       if (error) {
         return res.json({ error })
       }
       const filtered = files.filter(file => file.isFile()).map(file => file.name)
-      return res.json({error, files: filtered})
-    }) 
+      return res.json({ error, files: filtered })
+    })
   })
 
   app.get('/plugin/assets/index', cors, (req, res) => {
-
-    const walk = (dir) => {
-      const files = fs.readdirSync(path.join(argv.assets, dir), {withFileTypes: true})
-      return files.map((file) => {
-        if (!file.name.startsWith('.')) {
-          if (file.isFile()) {
-            return { file: path.join(path.sep, dir, file.name), size: fs.statSync(path.join(argv.assets, dir, file.name)).size }
+    const walk = dir => {
+      const files = fs.readdirSync(path.join(argv.assets, dir), { withFileTypes: true })
+      return files
+        .map(file => {
+          if (!file.name.startsWith('.')) {
+            if (file.isFile()) {
+              return {
+                file: path.join(path.sep, dir, file.name),
+                size: fs.statSync(path.join(argv.assets, dir, file.name)).size,
+              }
+            }
+            if (file.isDirectory()) {
+              return walk(path.join(dir, file.name)).flat()
+            }
+            return []
+          } else {
+            return []
           }
-          if (file.isDirectory()) {
-            return walk(path.join(dir, file.name)).flat()
-          }
-          return []
-        } else {
-          return []
-        }
-      }).flat()
+        })
+        .flat()
     }
 
     return res.json(walk(''))
   })
 
   app.post('/plugin/assets/upload', authorized, upload.any(), (req, res) => {
-    const assetsPath = path.join(argv.assets, (req.body.assets || '').match(/([\w\/-]*)/)[1])
+    const assetsPath = path.join(argv.assets, (req.body.assets || '').match(/([\w/-]*)/)[1])
     fs.mkdirSync(assetsPath, { recursive: true })
     let errors = []
-    req.files.forEach(((element) => {
+    req.files.forEach(element => {
       const uploaded = element.path
       const assetDestination = path.join(assetsPath, element.originalname)
-      fs.rename(uploaded, assetDestination, (err) => {
+      fs.rename(uploaded, assetDestination, err => {
         if (err) {
           errors.push(`rename failed for ${element.originalname}`)
         }
       })
-    }))
+    })
     if (errors.length > 0) {
       res.status(500).send(errors.join('\n'))
     } else {
@@ -80,9 +83,9 @@ const startServer = (params) => {
   app.post('/plugin/assets/delete', authorized, (req, res) => {
     const file = path.basename(req.query.file || '')
     if (file) {
-      const assets = (req.query.assets || '').match(/([\w\/-]*)/)[1]
+      const assets = (req.query.assets || '').match(/([\w/-]*)/)[1]
       const toRemove = path.join(argv.assets, assets, file)
-      fs.unlink(toRemove, (err) => {
+      fs.unlink(toRemove, err => {
         if (err) {
           res.status(500).send(err.message)
         } else {
@@ -95,5 +98,4 @@ const startServer = (params) => {
   })
 }
 
-
-module.exports = { startServer }
+export { startServer }
